e42bb6da5abc1f8a23c7f6e37a1b98966c4ca72e
Vincent Lee <vincentlee377@hotmail.com>
2017-06-19 22:06:13 -0400

    cycle through colors and modified delete

--- GenerateCapsules.cs
+++ GenerateCapsules.cs
@@ -7,9 +7,10 @@ using System.Collections;
 public class GenerateCapsules : MonoBehaviour {
     public SteamVR_TrackedObject leftCtrl;
     ArrayList list=new ArrayList();
-    int counter = 0;
+    int counter = -1;
     public GameObject cam;
-
+    int lcount = -1;
+    Colors[] label = {Colors.blue, Colors.green, Colors.magenta, Colors.yellow, Colors.orange };
 
     private void Update()
     {
@@ -32,19 +33,45 @@ public class GenerateCapsules : MonoBehaviour {
         // touchpad click right cycles and teleports
         if (touchpad.x > .7f && deviceLeft.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
         {
+            counter++;
             counter %= list.Count;
             cam.transform.position = ((GameObject)(list[counter])).transform.position;
-            counter++;
             Debug.Log("Size: " + list.Count);
             Debug.Log(" Counter: " + counter);
         }
         //touchpad click down destroys the pad your at
-        if (touchpad.y < -.7f && deviceLeft.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad) && counter>0)
+        if (touchpad.y < -.7f && deviceLeft.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
         {
-            counter--;
-            Debug.Log("trying to destroy and counter:" + counter);
-            Destroy((GameObject)(list[counter]));
-            list.RemoveAt(counter);
+            //destroy the touchpad your on?
+            if(cam.transform.position == ((GameObject)(list[list.Count - 1])).transform.position){
+                Destroy((GameObject)(list[list.Count-1]));
+                list.RemoveAt(list.Count-1);
+            }
+            //destroy the touchpad you teleporte to
+            else if (counter > -1)
+            {
+                Debug.Log("trying to destroy and counter:" + counter);
+                Destroy((GameObject)(list[counter]));
+                list.RemoveAt(counter);
+            }
+            counter %= list.Count;
+        }
+
+        //change the color of the padd
+        if (touchpad.x < -.7f && deviceLeft.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            lcount++;
+            lcount %= 6;
+            //change the color of the pad youre on
+            if (cam.transform.position == ((GameObject)(list[list.Count - 1])).transform.position)
+            {
+                ((GameObject)(list[list.Count - 1])).GetComponent<Renderer>().material.color=(Color)(labels[lcount]);
+            }
+            //change the color of the pad you teleported to
+            else if (counter>-1)
+            {
+                ((GameObject)(list[counter])).GetComponent<Renderer>().material.color = (Color)(labels[lcount]);
+            }
         }
     }
     
